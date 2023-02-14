97904c8fdb178986693a52d8f2d1f373cf41ebf7
direktor2007 <>
2023-02-14 22:13:38 +0700

    task05

---
+++ seminar/task02/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит случайное 
+//трехзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+// 1. Вариант решения
+// Random rand = new Random ();
+// int number = rand.Next (100, 1000);
+// System.Console.WriteLine($"Случайное число:{number}");
+
+// System.Console.WriteLine($"{number / 100}{number % 10}");
+
+// 2/ Вариант решения чтобы переменная была целочисленная
+Random rand = new Random ();
+int number =  new Random().Next (100, 1000);
+System.Console.WriteLine($"Случайное число:{number}");
+
+//System.Console.WriteLine($"{number / 100}{number % 10}");
+number = ((number / 100) * 10) + (number % 10);
+System.Console.WriteLine($"{number}");
+
+
+
